f449a6435d73f15821371fdf5a4b159a60e580fc
AllPoland <52187288+AllPoland@users.noreply.github.com>
2026-01-09 14:17:17 -0700

    add animation component

---
+++ Assets/__Scripts/Animation/UIAnimator.cs
@@ -0,0 +1,103 @@
+using System;
+using DG.Tweening;
+using UBUI.Serialization;
+using UnityEngine;
+
+namespace UBUI.Animation
+{
+    [Serializable]
+    public class UIAnimatorData : SerializableData
+    {
+        public float duration = 0.2f;
+        public UIState eventState = UIState.None;
+        public Vector2 positionOffset = Vector2.zero;
+        public bool fade = false;
+        public Ease easing = Ease.OutQuad;
+
+        public SerializedBehaviour<CanvasGroup> canvasGroup;
+    }
+
+
+    [RequireComponent(typeof(RectTransform))]
+    public class UIAnimator : SerializableComponent<UIAnimatorData>
+    {
+        private RectTransform rectTransform;
+        private Vector2 startPos;
+        private Vector2 endPos;
+
+
+        public void PlayAnimation()
+        {
+            rectTransform.DOAnchorPos(endPos, Data.duration).SetEase(Data.easing);
+
+            if(Data.fade && Data.canvasGroup.Value)
+            {
+                Data.canvasGroup.Value.DOFade(0f, Data.duration);
+            }
+        }
+
+
+        public void PlayAnimationReverse()
+        {
+            rectTransform.DOAnchorPos(startPos, Data.duration).SetEase(Data.easing);
+
+            if(Data.fade && Data.canvasGroup.Value)
+            {
+                Data.canvasGroup.Value.DOFade(1f, Data.duration);
+            }
+        }
+
+
+        public void HandleNewState(UIState oldState, UIState newState)
+        {
+            if(Data.eventState == UIState.None)
+            {
+                return;
+            }
+
+            if(oldState == Data.eventState && newState != Data.eventState)
+            {
+                PlayAnimation();
+            }
+            else if(newState == Data.eventState && oldState != Data.eventState)
+            {
+                PlayAnimationReverse();
+            }
+        }
+
+
+        public void Init()
+        {
+            rectTransform = (RectTransform)transform;
+            Data.canvasGroup.FindValue(rectTransform);
+
+            startPos = rectTransform.anchoredPosition;
+            endPos = startPos + Data.positionOffset;
+        }
+
+
+        private void Awake()
+        {
+            Init();
+        }
+    }
+
+
+    public enum UIState
+    {
+        None,
+        TitleScreen,
+        MainMenu,
+        SongSelect,
+        DifficultySelect,
+        FilterSelect,
+        Leaderboard,
+        Modifiers,
+        FolioView,
+        ChallengeView,
+        CharacterSelect,
+        PlayerLeaderboard,
+        PlayerStats,
+        Any = int.MaxValue
+    }
+}
--- Assets/__Scripts/Serialization/SerializedReference.cs
+++ Assets/__Scripts/Serialization/SerializedReference.cs
@@ -21,6 +21,71 @@ namespace UBUI.Serialization
                 return true;
             }
 
+            if(owner.gameObject.name == name)
+            {
+                Value = owner.GetComponent<T>();
+                return true;
+            }
+
+            Transform transform = owner.Find(name);
+            if(!transform)
+            {
+                // Recurse down the hierarchy until we find the object
+                foreach(Transform child in owner)
+                {
+                    if(FindValue(child))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            Value = transform.GetComponent<T>();
+            return true;
+        }
+
+
+        public void UpdateName()
+        {
+            if(!Value)
+            {
+                name = "";
+            }
+            else name = Value.gameObject.name;
+        }
+
+
+        [OnSerializing]
+        internal void PreSerialize(StreamingContext context)
+        {
+            UpdateName();
+        }
+    }
+
+
+    [Serializable]
+    public class SerializedBehaviour<T> where T : Behaviour
+    {
+        [JsonIgnore] public T Value;
+
+        [HideInInspector] public string name = "";
+
+
+        public bool FindValue(Transform owner)
+        {
+            if(Value)
+            {
+                // No need to search for the value again
+                return true;
+            }
+
+            if(owner.gameObject.name == name)
+            {
+                Value = owner.GetComponent<T>();
+                return true;
+            }
+
             Transform transform = owner.Find(name);
             if(!transform)
             {
